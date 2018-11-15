61023b606617bf5b53155ba4cb4c4858604aebfd
Megan Fago <meganlaynefago@gmail.com>
2018-11-15 00:00:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)!".upcase
+  number -= 1
+end
+return "HAPPY NEW YEAR!".upcase
+end
 
-def countdown
+ def countdown_with_sleep(sleep)
+   sleep 5
 end
